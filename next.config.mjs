4322c0c4b0695bf03962343229ee6d7c72568cc1
Willian Gomes <146601824+WillianGomes949@users.noreply.github.com>
2025-09-29 15:45:49 -0300

    proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/linhas/:path*',
+        destination: 'http://gistapis.etufor.ce.gov.br:8081/api/linhas/:path*',
+      },
+      {
+        source: '/api/horarios/:path*',
+        destination: 'http://gistapis.etufor.ce.gov.br:8081/api/horarios/:path*',
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
