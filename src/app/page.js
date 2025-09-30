4a0e96e9b6b9feebbc935610fbb020c950bfe8af
Willian Gomes <146601824+WillianGomes949@users.noreply.github.com>
2025-09-30 14:11:04 -0300

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -84,74 +84,76 @@ export default function BusScheduleApp() {
   };
 
   return (
-    <main className="flex justify-center items-center min-h-screen bg-slate-50 dark:bg-slate-900 text-slate-700 dark:text-slate-300 font-sans p-2">
-      <NetworkStatus />
-      <div className="w-full max-w-4xl md:max-w-[80%] bg-white dark:bg-slate-800 rounded-2xl shadow-2xl p-2 md:p-8 space-y-6">
-        {/* Cabeçalho */}
-        <div className="text-center">
-          <h1 className="text-xl flex gap-2 md:text-3xl font-bold text-lime-600 dark:text-lime-400 items-center justify-center">
-            <RiBusFill size={40} />
-            Consulta de Horários
-          </h1>
-          <p className="text-sm text-slate-500 dark:text-slate-400 mt-2">
-            Veja os horários da sua linha de ônibus. API oficial da Etufor.
-          </p>
-        </div>
-        <div></div>
+    <>
+      <main className="flex justify-center items-center min-h-screen bg-slate-50 dark:bg-slate-900 text-slate-700 dark:text-slate-300 font-sans p-2">
+        <NetworkStatus />
+        <div className="w-full max-w-4xl md:max-w-[80%] bg-white dark:bg-slate-800 rounded-2xl shadow-2xl p-2 md:p-8 space-y-6">
+          {/* Cabeçalho */}
+          <div className="text-center">
+            <h1 className="text-xl flex gap-2 md:text-3xl font-bold text-lime-600 dark:text-lime-400 items-center justify-center">
+              <RiBusFill size={40} />
+              Consulta de Horários
+            </h1>
+            <p className="text-sm text-slate-500 dark:text-slate-400 mt-2">
+              Veja os horários da sua linha de ônibus. API oficial da Etufor.
+            </p>
+          </div>
+          <div></div>
 
-        {/* Formulário de Busca */}
-        <SearchForm
-          linha={linha}
-          data={data}
-          linhas={linhas}
-          loading={loading}
-          loadingLinhas={loadingLinhas}
-          onLinhaChange={setLinha}
-          onDataChange={setData}
-          onBuscarHorarios={handleBuscarHorarios}
-          onSelecionarLinha={handleSelecionarLinha}
-        />
-        <div className="flex justify-between items-center">
-            
+          {/* Formulário de Busca */}
+          <SearchForm
+            linha={linha}
+            data={data}
+            linhas={linhas}
+            loading={loading}
+            loadingLinhas={loadingLinhas}
+            onLinhaChange={setLinha}
+            onDataChange={setData}
+            onBuscarHorarios={handleBuscarHorarios}
+            onSelecionarLinha={handleSelecionarLinha}
+          />
+          <div className="flex justify-between items-center">
             <DownloadPDF
               dados={dados}
               linhaSelecionada={linhaSelecionada}
               data={data}
             />
           </div>
-        {/* Seção de Resultados */}
-        <ResultsSection
-          dados={dados}
-          loading={loading}
-          error={error}
-          linhaSelecionada={linhaSelecionada}
-        />
-      </div>
+          {/* Seção de Resultados */}
+          <ResultsSection
+            dados={dados}
+            loading={loading}
+            error={error}
+            linhaSelecionada={linhaSelecionada}
+          />
+        </div>
 
-      <style jsx global>{`
-        .custom-scrollbar::-webkit-scrollbar {
-          width: 8px;
-        }
-        .custom-scrollbar::-webkit-scrollbar-track {
-          background: transparent; /* slate-50 */
-        }
-        .dark .custom-scrollbar::-webkit-scrollbar-track {
-          background: none; /* slate-800 */
-        }
-        .custom-scrollbar::-webkit-scrollbar-thumb {
-          background: #cbd5e1; /* slate-300 */
-          border-radius: 4px;
-        }
-        .dark .custom-scrollbar::-webkit-scrollbar-thumb {
-          background: #475569; /* slate-600 */
-        }
-        .custom-scrollbar::-webkit-scrollbar-thumb:hover {
-          background: #94a3b8; /* slate-400 */
-        }
-        .dark .custom-scrollbar::-webkit-scrollbar-thumb:hover {
-          background: #64748b; /* slate-500 */
-        }
-      `}</style>
-    </main>
+        <style jsx global>{`
+          .custom-scrollbar::-webkit-scrollbar {
+            width: 8px;
+          }
+          .custom-scrollbar::-webkit-scrollbar-track {
+            background: transparent; /* slate-50 */
+          }
+          .dark .custom-scrollbar::-webkit-scrollbar-track {
+            background: none; /* slate-800 */
+          }
+          .custom-scrollbar::-webkit-scrollbar-thumb {
+            background: #cbd5e1; /* slate-300 */
+            border-radius: 4px;
+          }
+          .dark .custom-scrollbar::-webkit-scrollbar-thumb {
+            background: #475569; /* slate-600 */
+          }
+          .custom-scrollbar::-webkit-scrollbar-thumb:hover {
+            background: #94a3b8; /* slate-400 */
+          }
+          .dark .custom-scrollbar::-webkit-scrollbar-thumb:hover {
+            background: #64748b; /* slate-500 */
+          }
+        `}</style>
+      </main>
+      <footer>willian.dev © 2025</footer>
+    </>
   );
 }
